4be207deb9886422ce75de15e62c109090d2f84f
Brandon <kounse@gmail.com>
2022-08-20 13:43:00 -0400

    Finished exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num === 0) {
+        return '';
+    }
+    else if (num < 1) {
+        return "ERROR";
+    }
+    else {
+        let result = '';
+        for (i = 0; i < num; i++) {
+            result += string;
+        }
+    
+        return result;
+    }
+    
 };
 
 // Do not edit below this line
