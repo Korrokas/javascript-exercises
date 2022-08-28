de04fc2230ef4eb4e114932ef7cd838ddb8d7136
Brandon <kounse@gmail.com>
2022-08-28 15:50:43 -0400

    Finished exercise 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  const celsius = (num - 32) * (5/9);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  const fahrenheit = (num * (9/5)) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
