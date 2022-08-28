927e877a656546ae9c4e3546e0352553cb4863b8
Brandon <kounse@gmail.com>
2022-08-28 13:51:05 -0400

    Comleted TDD exercise 05/sumALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    const values = [];
+    let sum = 0;
+    let start = Math.min(a, b);
+    const end = Math.max(a, b);
+
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    else if (typeof(a) != "number" || typeof(b) != "number") {
+        return "ERROR";
+    }
+    else {
+        while (start <= end) {
+            values.push(start);
+            start++;
+        }
+    
+        for (let i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+        return sum;
+    }
 
 };
 
