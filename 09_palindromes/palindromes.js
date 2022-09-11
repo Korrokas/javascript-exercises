a47ff78c866bd0733cbc3d84888f7009b41ef9f4
Brandon <kounse@gmail.com>
2022-09-11 11:17:19 -0400

    Finished exercise 09: palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    const regEx = /\w/g;
+    const regExWord = words.match(regEx).join('').toLowerCase();
 
+    const palindromeWord = () => {
+        const reverse = regExWord.split('').reverse().join('').toLowerCase();
+        return reverse;
+    }    
+    
+    return palindromeWord() === regExWord;
 };
 
 // Do not edit below this line
