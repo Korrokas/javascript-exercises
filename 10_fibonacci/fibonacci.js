fff79f637425ca00d7db58d846b3f0852a5f1f3d
Brandon <kounse@gmail.com>
2022-09-11 11:56:29 -0400

    Completed javascript exercise 10: fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fibSeries = [0, 1];
 
+    if (num < 0) {
+        return 'OOPS';
+    }
+    for (let i = fibSeries.length; i < num + 1; i ++) {
+        let nextFibNum = (fibSeries[i - 1] + fibSeries[i - 2]);
+        fibSeries.push(nextFibNum);
+    }
+    console.log(fibSeries);
+    console.log(num);
+    return fibSeries[num];
 };
 
 // Do not edit below this line
