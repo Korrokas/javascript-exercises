df9e4314cdb81a4b00cb97c1ee7a412510b2c5be
Brandon <kounse@gmail.com>
2022-09-10 21:04:10 -0400

    finished TDD 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -28,8 +28,14 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  for (let i = a - 1; i > 0; i--) {
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
