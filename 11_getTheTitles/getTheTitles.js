57e321f7a83465811e4f8e3e916cec6c438c8a36
Brandon <kounse@gmail.com>
2022-09-11 13:07:48 -0400

    Completed exercise for pushing title objects to new array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    const titlesArray = [];
+
+    books.filter(books => titlesArray.push(books.title));
+    return titlesArray;    
 };
 
 // Do not edit below this line
