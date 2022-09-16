cfda992938ca0ec6bfef32bed8eddb31f6adf0cb
Brandon <kounse@gmail.com>
2022-09-16 04:08:10 -0400

    Created a forEach and sort array function to find the oldest person in an array of people objects.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
+people.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth)
+people.sort((p1, p2) => (p1.age < p2.age) ? 1 : -1)
+
+const oldest = people[0];
+
+
+console.log(people);
+return oldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
