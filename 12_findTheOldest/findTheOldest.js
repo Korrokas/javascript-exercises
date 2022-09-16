8b03cfe3639e5656f83233f1e226e8aab5e07277
Brandon <kounse@gmail.com>
2022-09-16 04:22:03 -0400

    Created a new Date object to add today's year if a person object did not have a yearOfDeath property.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,12 +1,18 @@
 const findTheOldest = function(people) {
 
-people.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth)
+const date = new Date();
+const currentYear = date.getFullYear();
+
+people.forEach(person => {
+    if (!person.hasOwnProperty('yearOfDeath')) {
+        person.yearOfDeath = currentYear;
+    }
+    person.age = person.yearOfDeath - person.yearOfBirth
+})
+
 people.sort((p1, p2) => (p1.age < p2.age) ? 1 : -1)
 
 const oldest = people[0];
-
-
-console.log(people);
 return oldest;
 }
 
